975a8b493d3fbed33434b8095bc4825c7240124c
yossihub <85499844+yossihub@users.noreply.github.com>
2021-06-07 17:21:56 +0300

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,19 @@
+<?php
+	header("refresh:2; url=website.html");
+?>
+<!DOCTYPE html>
+<html lang="he">
+<head>
+	<link rel="stylesheet" href="sweetalert2.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css /">
+	<link rel="stylesheet" href="font/css/all.min.css"> 
+	<meta charset="utf-8"/>
+	<title> ברוכים הבאים לאתר </title>
+</head>
+<body dir="rtl">
+	<h1> 
+	<i class="fas fa-spinner"></i>
+	 דף זה נטען...
+	</h1>
+</body>
+</html>
